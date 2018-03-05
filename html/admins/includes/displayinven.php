a8b23457db30a1846966b80cb6f680e49b394f4d
Jess <root@ip-172-31-1-164.us-east-2.compute.internal>
2018-03-05 09:31:32 +0000

    Some data into webpage

--- html/admins/includes/displayinven.php
+++ html/admins/includes/displayinven.php
@@ -1,25 +1,29 @@
 <?php
-
 	include_once '../../dbh.php';
-
 	if(isset($_POST['btnclick']) && $_POST['btnclick']=='b2')
 	{
-
-
 	//$btnoption= $_POST['btnclick'];
-
-
-		$sql= 'select inventory.iid as "ID", item_info.itemname as "Name", inventory.iavailable as "Availability", inventory.icondition as "Condition", item_info.itemprice as "Price" from inventory join item_info on inventory.itype=item_info.itemid;' ;
+		$sql= 'select inventory.iid, item_info.itemname, inventory.iavailable, inventory.icondition, item_info.itemprice from inventory join item_info on inventory.itype=item_info.itemid;' ;
 		$response=$conn->query($sql);
 
-
-
+		  echo "<table border='1'>
+			<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Availability</th>
+			<th>Condition</th>
+			<th>Price</th>
+			</tr>";
 		while($result=$response->fetch_assoc())
 		{
-			var_dump($result);
-			echo PHP_EOL;
-
+			echo "<tr>";
+			echo "<td>" . $result['iid'] . "</td>";
+			echo "<td>" . $result['itemname'] . "</td>";
+			echo "<td>" . $result['iavailable'] . "</td>";  	
+		   	echo "<td>" . $result['icondition'] . "</td>";
+			echo "<td>" . $result['itemprice']. "</td>";  
 		}
 
+		echo "</table>";
 	}
 ?>
